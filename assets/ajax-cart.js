15a1e8b191f38badbf6160cec6d65ee407c650a3
khumoyun-eddie <eddie.freelance21@gmail.com>
2022-09-27 23:57:25 +0900

    cart ready

--- assets/ajax-cart.js
+++ assets/ajax-cart.js
@@ -24,14 +24,13 @@ class Cart {
     return this.fetchAPI("change.js", formData);
   }
 
+
   async getCartDetails() {
     const response = await fetch(`/cart.js`);
     return response.json();
   }
 
-  deleteItem(line) {
-    this.changeItem({ line: line, quantity: 0 }).then((res) => this.renderCartItems(res));
-  }
+  
 
   async getProduct(handle) {
     const response = await fetch(`/products/${handle}.js`);
@@ -50,30 +49,40 @@ class Cart {
   }
 
   renderCartItems(cartDetails) {
+    console.log(cartDetails)
+    if(cartDetails.item_count !== 0){
+      document.querySelector('.side-cart__full').style.display = 'block'
+      document.querySelector('.side-cart__empty').style.display= 'none'
+    }else{
+      document.querySelector('.side-cart__full').style.display = 'none'
+      document.querySelector('.side-cart__empty').style.display= 'block'
+      return
+    }
     const cartItemsWrapper = document.querySelector(".side-cart__products-list");
+    
     cartItemsWrapper.innerHTML = "";
- 
+    // if(cartDetails.length === 0) return
     for (let item of cartDetails.items) {
       const template = `
-      <div class="side-cart__product item">
-      <a href="${ item.url}" class="item__img-wrapper">
-        <img src="${item.image}" class="item__img" alt="${ item.title }">
+      <div class="side-cart__product item" data-id="${item.id}" data-amount="${item.quantity}" >
+      <a href="${item.url}" class="item__img-wrapper">
+        <img src="${item.image}" class="item__img" alt="${item.title}">
       </a>
       <div class="item__details">
         <div class="details-wrapper">
-          <a href="${ item.url }" class="item__title">${ item.product_title }</a>
-          <p class="item__price">${ this.formatter.format(item.price / 100) }</p>
+          <a href="${item.url}" class="item__title">${item.product_title}</a>
+          <p class="item__price">${this.formatter.format(item.price / 100)}</p>
         </div>
         <div class="details-wrapper">
-          <p class="item__variant-type">${item.variant_title }</p>
-          <a href="/cart/change?line={{ forloop.index }}&amp;quantity=0" class="item__remove">
-           <img src="https://cdn.shopify.com/s/files/1/0601/7853/0436/files/icon-remove.svg?v=1664203933" alt="" />
-          </a>
+          <p class="item__variant-type">${item.variant_title}</p>
+       
+           <img data-operator="remove" class="item__remove item__control" src="https://cdn.shopify.com/s/files/1/0601/7853/0436/files/icon-remove.svg?v=1664203933" alt="" />
+        
         </div>
         <div class="item__controllers">
-          <span class="item__minus item__control">-</span>
+          <img src="https://cdn.shopify.com/s/files/1/0601/7853/0436/files/icon-minus.svg?v=1664203923" data-operator="minus" class="item__minus item__control" />
           <span class="item__count">${item.quantity}</span>
-          <span class="item__add item__control">+</span>
+          <img src="https://cdn.shopify.com/s/files/1/0601/7853/0436/files/icon-add.svg?v=1664203884" data-operator="plus" class="item__add item__control" />
         </div>
       </div>
     </div>
@@ -120,6 +129,35 @@ class Cart {
       });
     });
   }
+  
+  increaseItemAmount({ itemID, itemAmount }) {
+   
+    const formData = {
+      id: itemID,
+      quantity:Number(itemAmount) + 1
+    };
+
+    this.changeItem(formData).then(() => this.updateCart());
+  }
+  
+  decreaseItemAmount({ itemID, itemAmount }) {
+ 
+    const formData = {
+      id: itemID,
+      quantity:Number(itemAmount) - 1
+    };
+  
+    this.changeItem(formData).then(() => this.updateCart());
+  }
+
+  deleteItem({itemID}) {
+    const formData = {
+      id: itemID,
+      quantity:0
+    };
+    this.changeItem(formData).then(() => this.updateCart());
+  }
+
 }
 
 const sideCart = new Cart();
@@ -129,7 +167,6 @@ const addToCartBtn = document.querySelector(".product-details__add-cart");
 
 openCartIcon.addEventListener("click", () => {
   sideCart.toggleCart();
-
 });
 closeIcon.addEventListener("click", () => {
   sideCart.closeModal();
@@ -139,11 +176,21 @@ addToCartBtn.addEventListener("click", function (e) {
   sideCart.addToCart();
 });
 
-const itemWrapper = document.querySelector('.side-cart__products-list')
-itemWrapper.addEventListener('click', function(e){
-  const plusBtn = e.target.closest('.item__add')
-  const minusBtn = e.target.closest('.item__minus')
-  plusBtn.addEventListener('click',()=>{
-    sideCart.addToCart()
-  })
-})
+const itemWrapper = document.querySelector(".side-cart__products-list");
+itemWrapper?.addEventListener("click", function (e) {
+  const target = e.target.closest(".item__control");
+  if(!target) return
+  const operator = target.dataset.operator
+  const cartItem = target.closest('.item')
+  console.log(cartItem)
+
+  if(operator === 'plus') {
+    sideCart.increaseItemAmount({itemID:cartItem.dataset.id, itemAmount: cartItem.dataset.amount})
+  }  
+  if(operator === 'minus') {
+    sideCart.decreaseItemAmount({itemID:cartItem.dataset.id, itemAmount: cartItem.dataset.amount})
+  }
+  if(operator === 'remove') {
+    sideCart.deleteItem({itemID:cartItem.dataset.id})
+  }
+});
